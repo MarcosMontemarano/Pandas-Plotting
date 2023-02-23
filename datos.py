623ce70e663b92ee2f7ca0e5dd2a659afa0e8d78
Marcos <marcosmontemarano@hotmail.com>
2023-02-23 18:26:12 -0300

    2do commit

--- datos.py
+++ datos.py
@@ -6,6 +6,7 @@ import numpy as np
 
 df = pd.read_excel('mediciones.xlsx')
 
+# Vector de frecuencias
 frecuencia = df['frecuencia']
 
 # Angulos desde -90° a -15°
@@ -17,17 +18,29 @@ ang0 = pd.DataFrame.to_numpy(df.iloc[[0],[1]])
 # Angulos desde 15° a 90°
 ang2 = pd.DataFrame.to_numpy(df.iloc[[0],[2,3,4,5,6,7]])
 
+# Vector de angulos
 angulos_1 = np.hstack((ang1,ang0,ang2))
-# angulo_0 = df['angulo_0']
-# angulo_15 = df['angulo_15']
-# angulo_30 = df['angulo_30']
-# angulo_45 = df['angulo_45']
-# angulo_60 = df['angulo_60']
-# angulo_75 = df['angulo_75']
-# angulo_90 = df['angulo_90']
 
+# Crear matriz de 13x806
 X_frec , Y_ang = np.meshgrid(frecuencia, angulos_1)
 
 # Matriz de 13x806
+magnitudes = np.zeros(np.shape(X_frec))
 
-magnitudes = np.matrix()
+"""
+Recorrido de cada fila de Magnitudes (i), de 0 a 13
+y de las columnas (j) tiene que ser de -90° a 90°
+es decir, de la columna -90°, hasta la 0° y vuelta hasta +90°.
+Esto es, que recorra las columnas del dataframe 
+7,6,5,4,3,2,1,-6,-5,-4,-3,-2,-1. Este vector será el que
+haga recorrer j
+"""
+vector1_j = np.arange((int(len(magnitudes)/2)+1),0,-1) # Desde 7 hasta 1
+
+vector2_j = np.arange(-(int(len(magnitudes)/2)),0,1) # Desde -6 hasta -1
+
+vector_j = np.hstack((vector1_j,vector2_j))
+
+for i in range(len(magnitudes)):
+    for j in vector_j:
+        magnitudes[i] = df.iloc[:,j]
